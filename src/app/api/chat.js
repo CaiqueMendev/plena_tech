d6c8e1679f4cb8e64c394988255ad7ed174594ff
Kaká <caiquemendes.dev@gmail.com>
2025-03-21 10:37:49 -0300

    api key set

--- src/app/api/chat.js
+++ src/app/api/chat.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: 'key_JZvhMO2VLFGfeF2M',
 });
 const openai = new OpenAIApi(configuration);
 
